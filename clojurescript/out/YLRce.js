388d23387ec3eee771a4fe56dd3aed3db09904a5
Erik Hinton <erik.hinton@nytimes.com>
2014-07-18 13:48:39 -0400

    ignore

--- clojurescript/out/YLRce.js
+++
@@ -1,7 +0,0 @@
-goog.provide('hello_js.core');
-goog.require('cljs.core');
-hello_js.core.popup_msg = (function popup_msg(msg){return send_alert(msg);
-});
-goog.exportSymbol('hello_js.core.popup_msg', hello_js.core.popup_msg);
-hello_js.core.popup_msg.call(null,"ClojureScript calling a global function defined in an external JavaScript library");
-hello_js.core.popup_msg.call(null,("ClojureScript: the time is now "+cljs.core.str.cljs$core$IFn$_invoke$arity$1((new Date()))));
