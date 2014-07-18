b6691f80c8fab55810ad494358967db20ea55f47
Erik Hinton <erik.hinton@nytimes.com>
2014-07-18 14:51:53 -0400

    elm

---
+++ elm/demo.elm
@@ -0,0 +1,16 @@
+module Stamper where
+ 
+import Mouse
+import Window
+ 
+main = lift2 renderStamps Window.dimensions clickLocations
+ 
+clickLocations = foldp (::) [] (sampleOn Mouse.clicks Mouse.position)
+ 
+renderStamps (w,h) locs =
+  let pentagon (x,y) =
+          ngon 5 20 |> filled (hsl 0.3 0.7 0.5)
+                    |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+                    |> rotate (toFloat x)
+  in  layers [ collage w h (map pentagon locs)
+             , plainText "Click to stamp a pentagon." ]
