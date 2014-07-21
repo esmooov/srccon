83c531095e0c7d4b118043954a3e6441bccf7ace
Erik Hinton <erik.hinton@nytimes.com>
2014-07-21 13:31:31 -0400

    es6

---
+++ es6/traceur/test.js
@@ -0,0 +1,35 @@
+
+/* DESTRUCTURING */
+
+function destruct() {
+    var [a,b,c] = [1,2,3]
+    console.log("A: ",a);
+    console.log("B: ",b);
+    console.log("C: ",c);
+}
+
+/* ITERATORS */
+
+function iterate() {
+    for (var element of [1,2,3]) {
+        console.log("I gotta",element);
+    }
+}
+
+function customIterator() {
+    
+    var myiterator = function*(mult){
+        var hopper = [1,2,3], i = 0;
+        while (i < 3){
+            yield (mult* hopper[i]);
+            i++;
+        }
+    }
+
+    var doubler = myiterator(2);
+
+    for (var element of doubler){
+        console.log("My doubler gave me",element);
+    }
+
+}
